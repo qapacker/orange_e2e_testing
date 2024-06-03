ae29ce4e94fb850f76b00c9136dcccbce33e1b1c
Gil, Mateo <mateo.gil@i2cat.net>
2024-06-03 16:32:37 +0200

    Update Cypress config and login test for session-1

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:8200',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Implementa eventos del nodo aquí
     },
   },
 });
--- cypress/e2e/1-Smoke-test/login.cy.js
+++ cypress/e2e/1-Smoke-test/login.cy.js
@@ -1,15 +1,15 @@
 /// <reference types="cypress" />
 
 describe('Login Test', () => {
-    it('should successfully login with valid credentials', () => {
-      cy.visit('http://localhost:8200');
-  
-      cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(Cypress.env('username'));
-      cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(Cypress.env('password'));
-      cy.get('.oxd-button').click();
-  
-      cy.url().should('include', '/dashboard');
-      cy.get('.oxd-topbar-header-title').should('contain', 'Dashboard');
-    });
+  it('should successfully login with valid credentials', () => {
+    cy.visit('/web/index.php/auth/login');
+
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(Cypress.env('username'));
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(Cypress.env('password'));
+    cy.get('.oxd-button').click();
+
+    cy.url().should('include', '/dashboard');
+    cy.get('.oxd-topbar-header-title').should('contain', 'Dashboard');
   });
+});
   
